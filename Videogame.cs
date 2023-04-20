98ea9d1db07d97f4c5b3e1bf9423fbb7f394150c
Francesco Limpias <francescolimpias@gmail.com>
2023-04-20 14:44:26 +0200

    Added base Videogame class

---
+++ Videogame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    internal class Videogame
+    {
+        /* ***************
+         * VIDEOGAME's PROPERTIES
+         */
+        public int ID { get; }
+        public string Name { get; set; }
+
+        //CONSTRUCTOR
+        internal Videogame(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        /* ***************
+         * VIDEOGAME's METHODS
+         */
+    }
+}
