515410dd3e9edd6bc068cdc5416bc334328f07b1
Francesco Limpias <francescolimpias@gmail.com>
2023-04-20 14:54:42 +0200

    Added Connection attempt @ Program.Main

--- Program.cs
+++ Program.cs
@@ -4,14 +4,15 @@ namespace adonet_db_videogame
 {
     internal class Program
     {
+        //Connection 
+        static readonly string connectionParameters = "Data Source=lcalhost;Initial Catalog=adonet-db-videogame;Integrated Security=True";
+        internal static readonly SqlConnection SQL = new SqlConnection(connectionParameters);
+
         static void Main(string[] args)
         {
-            string connectionParameters = "Data Source=localhost;Initial Catalog=adonet-db-videogame;Integrated Security=True";
 
             //Greet the user
             Console.WriteLine("Welcome to VIDEOGAMES MANAGER!");
-
-            using SqlConnection SQL = new SqlConnection(connectionParameters);
 
             //Attempt connection to database
             try
@@ -20,34 +21,15 @@ namespace adonet_db_videogame
             }
             catch
             {
-                Console.WriteLine("[ERR] Unable to connect to database!");
+                string ERROR = "Unable to connect to database!";
+                Console.WriteLine($"[ERR] {ERROR}");
+                throw new Exception(ERROR);
             }
 
-            string selectionQuery = "SELECT * FROM videogames";
 
-            //Build SQL command
-            using SqlCommand command = SQL.CreateCommand();
-            command.Connection = SQL;
-            command.CommandText = selectionQuery;
 
-            //Attempt command execution
-            try
-            {
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine(
-                        $"ID: {reader.GetInt64(0)}\r\n"
-                        + $"Name: {reader.GetString(1)}\r\n"
-                        + "--------------------\r\n\r\n"
-                        );
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("[ERR] " + e.Message);
-            }
+            //Close the connection
+            SQL.Close();
         }
     }
 }
